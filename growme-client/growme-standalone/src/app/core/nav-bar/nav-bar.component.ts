07819b165b6e85d3198ef294ba908c2ee8a3ad3a
DraganovTs <cvetomir_draganov@abv.bg>
2025-03-16 19:33:08 +0200

    front end keycloak secure

--- growme-client/growme-standalone/src/app/core/nav-bar/nav-bar.component.ts
+++ growme-client/growme-standalone/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { KeycloakService } from '../../services/keycloak.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,5 +11,25 @@ import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  isAuthenticated = false;
+  username:  string = '';
+  showRegisterModal: boolean = false;
 
+  constructor(private keycloakService: KeycloakService) {
+      this.isAuthenticated = !!this.keycloakService.isAuthenticated();
+      if (this.isAuthenticated) {
+          this.username = this.keycloakService.getUsername();
+      }
+  }
+
+  login() { this.keycloakService.login(); }
+  logout() { this.keycloakService.logout(); }
+  showRegisterOptions() {
+    this.showRegisterModal = true; 
+}
+
+register(role: 'BUYER' | 'SELLER') {
+    this.keycloakService.register(role);
+    this.showRegisterModal = false; 
+}
 }
--- growme-client/growme-standalone/src/main.ts
+++ growme-client/growme-standalone/src/main.ts
@@ -1,16 +1,23 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations'
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { KeycloakService } from './app/services/keycloak.service';
 
-bootstrapApplication(AppComponent , {
-  providers : [
-    provideAnimations(),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideRouter(routes)
-  ]
+const keycloakService = new KeycloakService();
 
-})
-  .catch((err) => console.error(err));
+keycloakService.init().then(() => {
+  console.log('Keycloak initialized successfully. Bootstrapping Angular...');
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(routes),
+        { provide: KeycloakService, useValue: keycloakService }
+    ]
+}).catch(err => console.error('Angular bootstrapping failed:', err));
+
+}).catch(err => console.error('Keycloak initialization failed:', err));
