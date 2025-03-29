1086c885a8a62973b5aff718714cc458aa5534c1
DraganovTs <cvetomir_draganov@abv.bg>
2025-03-29 12:31:07 +0200

    config and implement kafka consumer for keycloak service

--- keycloak-api/src/main/java/com/home/keycloak/api/config/KafkaConsumerConfig.java
+++ keycloak-api/src/main/java/com/home/keycloak/api/config/KafkaConsumerConfig.java
@@ -1,7 +1,50 @@
 package com.home.keycloak.api.config;
 
+import com.home.growme.common.module.dto.RoleAssignmentMessage;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.consumer.group-id:keycloak-role-service}")
+    private String groupId;
+
+
+    @Bean
+    public ConsumerFactory<String, RoleAssignmentMessage> roleAssignmentConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                new JsonDeserializer<>(RoleAssignmentMessage.class)
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, RoleAssignmentMessage> roleAssignmentListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, RoleAssignmentMessage> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(roleAssignmentConsumerFactory());
+        factory.setBatchListener(false);
+        return factory;
+    }
 }
