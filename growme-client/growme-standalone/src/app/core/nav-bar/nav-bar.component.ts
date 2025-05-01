fb519a1a6c63ca0748bd9f27c66656b43442a49b
DraganovTs <cvetomir_draganov@abv.bg>
2025-05-01 17:56:25 +0300

    add cart icon and numbre ritems

--- growme-client/growme-standalone/src/app/core/nav-bar/nav-bar.component.ts
+++ growme-client/growme-standalone/src/app/core/nav-bar/nav-bar.component.ts
@@ -2,6 +2,9 @@ import { CommonModule, NgIf } from '@angular/common';
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { KeycloakService } from '../../services/keycloak.service';
+import { CartService } from 'src/app/services/cart-service';
+import { Observable } from 'rxjs';
+import { ICart } from 'src/app/shared/model/cart';
 
 @Component({
   selector: 'app-nav-bar',
@@ -14,15 +17,16 @@ export class NavBarComponent {
   isAuthenticated = false;
   isSeller = false;
   username: string = '';
+  cart$!: Observable<ICart | null>; ;
  
 
-  constructor(private keycloakService: KeycloakService, private cdRef: ChangeDetectorRef) {
+  constructor(private keycloakService: KeycloakService, private cdRef: ChangeDetectorRef , private cartService: CartService) {
     this.keycloakService.isAuthenticated$.subscribe(authenticated => {
       this.isAuthenticated = authenticated;
       if (this.isAuthenticated) {
         this.username = this.keycloakService.getUsername() ?? '';
         this.isSeller = this.keycloakService.hasRole('SELLER'); 
-
+        this.cart$ = this.cartService.cart$;
       }
     });
   }
