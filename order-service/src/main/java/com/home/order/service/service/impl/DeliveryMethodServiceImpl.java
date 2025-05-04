c7f5f7453424596e52de536e8a5dfdb4e31a7358
DraganovTs <cvetomir_draganov@abv.bg>
2025-05-04 19:05:37 +0300

    delivery method

--- order-service/src/main/java/com/home/order/service/service/impl/DeliveryMethodServiceImpl.java
+++ order-service/src/main/java/com/home/order/service/service/impl/DeliveryMethodServiceImpl.java
@@ -1,5 +1,7 @@
 package com.home.order.service.service.impl;
 
+import com.home.order.service.mapper.DeliveryMethodMapper;
+import com.home.order.service.model.dto.DeliveryMethodDTO;
 import com.home.order.service.model.entity.DeliveryMethod;
 import com.home.order.service.repository.DeliveryMethodRepository;
 import com.home.order.service.service.DeliveryMethodService;
@@ -11,13 +13,19 @@ import java.util.List;
 public class DeliveryMethodServiceImpl implements DeliveryMethodService {
 
     private final DeliveryMethodRepository deliveryMethodRepository;
+    private final DeliveryMethodMapper deliveryMethodMapper;
 
-    public DeliveryMethodServiceImpl(DeliveryMethodRepository deliveryMethodRepository) {
+    public DeliveryMethodServiceImpl(DeliveryMethodRepository deliveryMethodRepository, DeliveryMethodMapper deliveryMethodMapper) {
         this.deliveryMethodRepository = deliveryMethodRepository;
+        this.deliveryMethodMapper = deliveryMethodMapper;
     }
 
     @Override
-    public List<DeliveryMethod> getAllDeliveryMethods() {
-        return deliveryMethodRepository.findAll();
+    public List<DeliveryMethodDTO> getAllDeliveryMethods() {
+        List<DeliveryMethodDTO> dtos = deliveryMethodRepository.findAll().stream()
+                .map(deliveryMethodMapper::mapDeliveryMethodToDeliveryMethodDTO)
+                .toList();
+
+        return dtos;
     }
 }
