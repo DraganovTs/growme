835e92fd7c1d328991eefe284d59af4fad86f83f
DraganovTs <cvetomir_draganov@abv.bg>
2025-05-06 17:59:24 +0300

    Add initial implementation for payment-service
    Introduced the `payment-service` with request/response models, service interface, and Spring Boot setup. Integrated the module into the multi-module project with necessary dependencies like Stripe and Lombok. Updated the ` to support payment processing fields.

--- payment-service/src/main/java/com/home/service/PaymentService.java
+++ payment-service/src/main/java/com/home/service/PaymentService.java
@@ -2,9 +2,10 @@ package com.home.service;
 
 import com.home.model.PaymentIntentRequest;
 import com.home.model.PaymentIntentResponse;
+import com.stripe.exception.StripeException;
 
 public interface PaymentService {
-    PaymentIntentResponse createOrUpdatePaymentIntent(PaymentIntentRequest request);
+    PaymentIntentResponse createOrUpdatePaymentIntent(PaymentIntentRequest request) throws StripeException;
 
     void handlePaymentSucceeded(String paymentIntentId);
 
---
+++ payment-service/src/main/java/com/home/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,53 @@
+package com.home.service.impl;
+
+import com.home.model.PaymentIntentRequest;
+import com.home.model.PaymentIntentResponse;
+import com.home.service.PaymentService;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+
+    @Override
+    public PaymentIntentResponse createOrUpdatePaymentIntent(PaymentIntentRequest request) throws StripeException {
+
+        Stripe.apiKey = "";
+        PaymentIntent intent;
+        List<String> paymentTypes = new ArrayList<>();
+        paymentTypes.add("card");
+        PaymentIntentCreateParams createParams =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(request.getAmount().multiply(BigDecimal.valueOf(100)).longValue())
+                        .setCurrency("usd")
+                        .addAllPaymentMethodType(paymentTypes)
+                        .build();
+        intent = PaymentIntent.create(createParams);
+
+        PaymentIntentResponse response = new PaymentIntentResponse();
+        response.setClientSecret(intent.getClientSecret());
+        response.setPaymentIntentId(intent.getId());
+        response.setStatus(intent.getStatus());
+
+        return response;
+    }
+
+    @Override
+    public void handlePaymentSucceeded(String paymentIntentId) {
+
+    }
+
+    @Override
+    public void handlePaymentFailed(String paymentIntentId) {
+
+    }
+}
