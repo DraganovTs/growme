1ee8a3eaaaed83361c81d588d934c4c3f4471b2a
DraganovTs <cvetomir_draganov@abv.bg>
2025-05-09 18:07:31 +0300

    Add exception handlers for payment and product errors
    Introduced specific handlers for PaymentProcessingException, InvalidProductException, and DeliveryMethodNotFoundException to improve error handling in the order service. Each handler logs the error and returns an appropriate response with relevant HTTP status codes and error codes. This enhances clarity and robustness in the exception handling process.

--- order-service/src/main/java/com/home/order/service/exception/OrderExceptionHandler.java
+++ order-service/src/main/java/com/home/order/service/exception/OrderExceptionHandler.java
@@ -15,12 +15,31 @@ import org.springframework.web.context.request.WebRequest;
 public class OrderExceptionHandler extends BaseExceptionHandler {
 
 
+    @ExceptionHandler(PaymentProcessingException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBasketNotFound(PaymentProcessingException ex, WebRequest request) {
+        log.warn("Payment is not valid: {}", ex.getMessage());
+        return buildErrorResponse(ex, request, HttpStatus.INTERNAL_SERVER_ERROR, "PAYMENT_PROCESSING_ERROR");
+    }
+
+    @ExceptionHandler(InvalidProductException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBasketNotFound(InvalidProductException ex, WebRequest request) {
+        log.warn("Product is not valid: {}", ex.getMessage());
+        return buildErrorResponse(ex, request, HttpStatus.BAD_REQUEST, "INVALID_PRODUCT");
+    }
 
     @ExceptionHandler(BasketNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleBasketNotFound(BasketNotFoundException ex, WebRequest request) {
         log.warn("Basket not found: {}", ex.getMessage());
         return buildErrorResponse(ex, request, HttpStatus.NOT_FOUND, "BASKET_NOT_FOUND");
     }
+
+
+    @ExceptionHandler(DeliveryMethodNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBasketNotFound(DeliveryMethodNotFoundException ex, WebRequest request) {
+        log.warn("Delivery Method not found: {}", ex.getMessage());
+        return buildErrorResponse(ex, request, HttpStatus.NOT_FOUND, "DELIVERY_METHOD_NOT_FOUND");
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDTO> handleGenericException(Exception ex, WebRequest request) {
