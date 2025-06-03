3e775b92ed4205b4d316347565b2aeae373ce1e1
DraganovTs <104751714+DraganovTs@users.noreply.github.com>
2025-06-03 20:53:45 +0300

    Enable Eureka Server and centralize configuration management
    Added @EnableEurekaServer annotation to activate Eureka Server functionality. Moved environment-specific configurations to the Config Server for better management and consistency across services. Adjusted profile-specific setup to streamline the dependency on the centralized configuration.

--- eureka-server/src/main/java/com/home/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/home/eureka/server/EurekaServerApplication.java
@@ -3,7 +3,10 @@ package com.home.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
     public static void main(String[] args) {
