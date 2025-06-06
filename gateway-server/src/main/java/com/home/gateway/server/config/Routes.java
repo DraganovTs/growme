220ce6410c40009bd9c0ed894fac5deefc3af0c3
DraganovTs <104751714+DraganovTs@users.noreply.github.com>
2025-06-06 20:24:00 +0300

    Refactor route paths and rewrite rules in Routes.java
    Updated route paths to remove redundant "api" segments and adjusted rewrite rules for consistency across services. This improves route clarity and aligns path structure with the desired API structure. Also added missing service names for better readability.

--- gateway-server/src/main/java/com/home/gateway/server/config/Routes.java
+++ gateway-server/src/main/java/com/home/gateway/server/config/Routes.java
@@ -12,38 +12,38 @@ public class Routes {
     public RouteLocator growMeRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 // USER-SERVICE
-                .route("user-service",r -> r.path("/growme/api/users/**")
-                        .filters(f -> f.rewritePath("/growme/api/users/(?<segment>.*)", "/${segment}"))
+                .route("user-service",r -> r.path("/growme/users/**")
+                        .filters(f -> f.rewritePath("/growme/users/(?<segment>.*)", "/${segment}"))
                         .uri("lb://USER-SERVICE"))
 
                 // PRODUCT-SERVICE
-                .route(p -> p.path("/growme/api/categories/**")
-                        .filters(f -> f.rewritePath("/growme/api/categories/(?<segment>.*)", "/${segment}"))
+                .route("product-service" ,p -> p.path("/growme/categories/**")
+                        .filters(f -> f.rewritePath("/growme/categories/(?<segment>.*)", "/api/categories/${segment}"))
                         .uri("lb://PRODUCT-SERVICE"))
-                .route(p -> p.path("/growme/api/owners/**")
-                        .filters(f -> f.rewritePath("/growme/api/owners/(?<segment>.*)", "/${segment}"))
+                .route("product-service",p -> p.path("/growme/owners/**")
+                        .filters(f -> f.rewritePath("/growme/owners/(?<segment>.*)", "/${segment}"))
                         .uri("lb://PRODUCT-SERVICE"))
-                .route(p -> p.path("/growme/api/products/**")
-                        .filters(f -> f.rewritePath("/growme/api/products/(?<segment>.*)", "/${segment}"))
+                .route("product-service",p -> p.path("/growme/products/**")
+                        .filters(f -> f.rewritePath("/growme/products/(?<segment>.*)", "/api/products/${segment}"))
                         .uri("lb://PRODUCT-SERVICE"))
 
                 // ORDER-SERVICE
-                .route(p -> p.path("/growme/api/basket/**")
-                        .filters(f -> f.rewritePath("/growme/api/basket/(?<segment>.*)", "/${segment}"))
+                .route("order-service",p -> p.path("/growme/basket/**")
+                        .filters(f -> f.rewritePath("/growme/basket(?<segment>/?.*)", "/api/basket${segment}"))
                         .uri("lb://ORDER-SERVICE"))
-                .route(p -> p.path("/growme/deliverymethods/**")
-                        .filters(f -> f.rewritePath("/growme/deliverymethods/?(?<segment>.*)", "/api/deliverymethods/${segment}"))
+                .route("order-service",p -> p.path("/growme/deliverymethods/**")
+                        .filters(f -> f.rewritePath("/growme/deliverymethods/?(?<segment>/?.*)", "/api/deliverymethods${segment}"))
                         .uri("lb://ORDER-SERVICE"))
-                .route(p -> p.path("/growme/api/orders/**")
-                        .filters(f -> f.rewritePath("/growme/api/orders/(?<segment>.*)", "/${segment}"))
+                .route("order-service",p -> p.path("/growme/orders/**","/growme/orders/**")
+                        .filters(f -> f.rewritePath("/growme/orders(?<segment>/?.*)", "/api/orders${segment}"))
                         .uri("lb://ORDER-SERVICE"))
 
                 //KEYCLOAK-ROLE-SERVICE
-                .route(p -> p.path("/growme/api/roles/**")
-                        .filters(f -> f.rewritePath("/growme/api/roles/(?<segment>.*)", "/${segment}"))
+                .route(p -> p.path("/growme/roles/**")
+                        .filters(f -> f.rewritePath("/growme/roles/(?<segment>.*)", "/api/roles/${segment}"))
                         .uri("lb://KEYCLOAK-ROLE-SERVICE"))
-                .route(p -> p.path("/growme/api/usersk/**")
-                        .filters(f -> f.rewritePath("/growme/api/usersk/(?<segment>.*)", "/${segment}"))
+                .route(p -> p.path("/growme/usersk/**")
+                        .filters(f -> f.rewritePath("/growme/usersk/(?<segment>.*)", "/api/usersk/${segment}"))
                         .uri("lb://KEYCLOAK-ROLE-SERVICE"))
                 .build();
     }
