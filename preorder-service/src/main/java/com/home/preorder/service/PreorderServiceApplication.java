8d7c921dec0f403851ecc6481a33830a08cd9de0
draganovTs <cvetomir_draganov@abv.bg>
2025-06-18 21:56:52 +0300

    Add Preorder Service to project
    Introduced a new `preorder-service` module with its own POM file, Spring Boot configuration, and initial starter dependencies. Updated parent POM and IDE encoding settings to include the service.

---
+++ preorder-service/src/main/java/com/home/preorder/service/PreorderServiceApplication.java
@@ -0,0 +1,11 @@
+package com.home.preorder.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreorderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PreorderServiceApplication.class, args);
+    }
+}
