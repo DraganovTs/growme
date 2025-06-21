1e72d61b0089a145e38c0c1b48af5fcb2584d32a
draganovTs <cvetomir_draganov@abv.bg>
2025-06-21 20:53:56 +0300

    Add initial PreorderController and service configuration
    - Implement basic endpoints (`createTask`, `getTask`, `updateTask`, `getTaskByUserId`) in `PreorderController`.
    - Configure `application.yml` with server port set to `8086`.

---
+++ preorder-service/src/main/java/com/home/preorder/service/controller/PreorderController.java
@@ -0,0 +1,35 @@
+package com.home.preorder.service.controller;
+
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/task",produces = MediaType.APPLICATION_JSON_VALUE)
+public class PreorderController {
+
+    @PostMapping
+    public ResponseEntity<?> createTask(){
+        return null;
+    }
+
+    @GetMapping("/{taskId}")
+    public ResponseEntity<?> getTask(@PathVariable String taskId){
+        return null;
+    }
+
+    @PutMapping("/{taskId}")
+    public ResponseEntity<?> updateTask(@PathVariable String taskId,
+                                        @RequestBody String status){
+        return null;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getTaskByUserId( @PathVariable String userId){
+        return null;
+    }
+
+
+
+}
