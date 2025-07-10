af7a1f16a1a99076e4fd3039dc673e9093ad7943
draganovTs <cvetomir_draganov@abv.bg>
2025-07-10 19:21:04 +0300

    bid list component

--- growme-client/growme-standalone/src/app/bids/bid-list/bid-list.component.ts
+++ growme-client/growme-standalone/src/app/bids/bid-list/bid-list.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BidCardComponent } from '../bid-card/bid-card.component';
+import { Bid } from 'src/app/shared/model/bid';
 
 @Component({
   selector: 'app-bid-list',
   standalone: true,
-  imports: [CommonModule , RouterModule],
+  imports: [CommonModule ,BidCardComponent],
   templateUrl: './bid-list.component.html',
   styleUrl: './bid-list.component.scss'
 })
 export class BidListComponent {
+  @Input() bids: Bid[] = [];
+  @Input() isTaskOwner = false;
+  @Input() taskStatus = '';
+  @Output() acceptBid = new EventEmitter<string>();
+  @Output() rejectBid = new EventEmitter<string>();
 
+  trackById(index: number, bid: Bid): string {
+    return bid.id;
+  }
 }
