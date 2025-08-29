ed383b38ce57c86f965d160898de967a311f6794
draganovTs <cvetomir_draganov@abv.bg>
2025-08-29 20:38:38 +0300

    Refactor `CorsConfig` to implement `WebMvcConfigurer` and enhance readability
    - Replaced anonymous `WebMvcConfigurer` bean with direct implementation in `CorsConfig`.
    - Introduced constants for mapping patterns, allowed origins, and methods to improve clarity and maintainability.

--- preorder-service/src/main/java/com/home/preorder/service/config/CorsConfig.java
+++ preorder-service/src/main/java/com/home/preorder/service/config/CorsConfig.java
@@ -1,23 +1,26 @@
 package com.home.preorder.service.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    private static final String MAPPING_PATTERN = "/**";
+    private static final String[] ALLOWED_ORIGINS = {
+            "http://localhost:4200"
+    };
+    private static final String[] ALLOWED_METHODS = {
+            "GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(MAPPING_PATTERN)
+                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedMethods(ALLOWED_METHODS)
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
