17b7c1391c453891fb8c2299d5cd5332cad63e06
DraganovTs <cvetomir_draganov@abv.bg>
2025-10-18 13:06:52 +0300

    Update application URLs, SSR configuration, and add growme-client service
    - Updated API URLs in `environments.ts` to use `gateway` service.
    - Changed Angular SSR output mode to `static` and adjusted build budgets in `angular.json`.
    - Added `growme-client` service to `docker-compose.yml` with port mapping and restart policy.
    - Removed obsolete `.idea/sqldialects.xml`.

--- growme-client/growme-standalone/src/app/environment/environments.ts
+++ growme-client/growme-standalone/src/app/environment/environments.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/growme/', 
-    keycloakUrl: 'http://tzetze:7080',
+    apiUrl: 'http://gateway:8080/growme/', 
+    keycloakUrl: 'http://keycloak:8080',
     keycloakRealm: 'grow-me',
     keycloakClientId: 'grow-me-client',
-    userApi: 'http://localhost:8080/growme/users',
-    productApi: 'http://localhost:8080/growme/products',
-    basketApi: 'http://localhost:8080/growme/basket',
-    orderApi: 'http://localhost:8080/growme/',
-    taskApi: 'http://localhost:8080/growme/',
+    userApi: 'http://gateway:8080/growme/users',
+    productApi: 'http://gateway:8080/growme/products',
+    basketApi: 'http://gateway:8080/growme/basket',
+    orderApi: 'http://gateway:8080/growme/',
+    taskApi: 'http://gateway:8080/growme/',
     pageSize: 6
 }
