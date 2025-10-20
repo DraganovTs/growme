114f1e0b84c29f1af0b716073be811ae8f110bd9
DraganovTs <cvetomir_draganov@abv.bg>
2025-10-20 21:03:12 +0300

    Update Keycloak and API URLs in docker-compose and environments
    - Updated `docker-compose.yml` to set consistent Keycloak URLs and HTTP port configurations.
    - Standardized `SPRING_SECURITY_OAUTH2_RESOURCESERVER_JWT_ISSUER_URI` and Keycloak-related URLs to use `http://keycloak:8080`.
    - Adjusted `environments.ts` to reflect hostname change from `gateway` to `tzetze` for API and Keycloak services.

--- growme-client/growme-standalone/src/app/environment/environments.ts
+++ growme-client/growme-standalone/src/app/environment/environments.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://gateway:8080/growme/', 
-    keycloakUrl: 'http://keycloak:8080',
+    apiUrl: 'http://tzetze:8080/growme/', 
+    keycloakUrl: 'http://tzetze:8080',
     keycloakRealm: 'grow-me',
     keycloakClientId: 'grow-me-client',
-    userApi: 'http://gateway:8080/growme/users',
-    productApi: 'http://gateway:8080/growme/products',
-    basketApi: 'http://gateway:8080/growme/basket',
-    orderApi: 'http://gateway:8080/growme/',
-    taskApi: 'http://gateway:8080/growme/',
+    userApi: 'http://tzetze:8080/growme/users',
+    productApi: 'http://tzetze:8080/growme/products',
+    basketApi: 'http://tzetze:8080/growme/basket',
+    orderApi: 'http://tzetze:8080/growme/',
+    taskApi: 'http://tzetze:8080/growme/',
     pageSize: 6
 }
